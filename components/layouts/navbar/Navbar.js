d125d9de051dbae265e58fbe3b859ff6a920de24
Sparkie95 <shind4r4_stalk@live.fr>
2022-10-16 00:04:28 +0200

    navbar templates right content items

--- components/layouts/navbar/Navbar.js
+++ components/layouts/navbar/Navbar.js
@@ -1,12 +1,204 @@
 export default function Navbar() {
   return (
-    <div>
-      <div>
-        <div>
-            <div>
-                <a>
+    <div className="navbar-main wf-section">
+      <div className="main-navigation w-nav">
+        <div className="page-padding">
+            <div className="container-large">
+                <a className="main-nav-brand w-nav-brand">
                     <h1>FE2_BONSAI</h1>
                 </a>
+                <nav className="main-navigation-menu w-nav-menu">
+                    <div className="main-nav-left">
+                        <div className="main-nav-link relative">
+                            <div className="main-nav-dropdown-toggle">Product</div>
+                            <div className="dropdown-arrow no-margin-right w-icon-dropdown-toggle"></div>
+                            <div className="main-nav-dropdown-wrapper hidden">
+                                <div className="main-nav-left-content">
+                                    <div className="main-nav-inside-dropdown-toggle arrow-bg">
+                                        <div className="main-nav-inside-content-wrapper">
+                                            <div className="main-nav-inside-dropdown-heading">Bonsai Workflow</div>
+                                            <div className="main-nav-inside-dropdown-subheading">Look professional, win more clients and manage your business from one place</div>
+                                        </div>
+                                    </div>
+                                    <a className="main-nav-inside-dropdown-toggle w-inline-block">
+                                        <div className="main-nav-inside-content-wrapper">
+                                            <div className="main-nav-inside-dropdown-heading">Bonsai Tax</div>
+                                            <div className="main-nav-inside-dropdown-subheading">"Track expenses, maximize tax write-offs, and estimate taxes without the headache"</div>
+                                        </div>
+                                    </a>
+                                    <a className="main-nav-inside-dropdown-toggle w-inline-block">
+                                        <div className="main-nav-inside-content-wrapper">
+                                            <div className="main-nav-inside-dropdown-heading">Bonsai Cash</div>
+                                            <div className="main-nav-inside-dropdown-subheading">Bonsai's all in-one financial hub: No fees and lightning fast payouts</div>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div className="main-nav-right-content">
+                                    <div className="side-dropdown-grid">
+                                        <a className="side-dropdown-link-block w-inline-block">
+                                            <div className="side-dropdown-inner-link">
+                                                <img />
+                                                <div className="side-dropdown-content">
+                                                    <div className="side-dropdown-heading">Invoices</div>
+                                                    <div className="side-dropdown-text">Get paid faster with automatic invoicing, online payments and more</div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a className="side-dropdown-link-block w-inline-block">
+                                            <div className="side-dropdown-inner-link">
+                                                <img />
+                                                <div className="side-dropdown-content">
+                                                    <div className="side-dropdown-heading">Proposals</div>
+                                                    <div className="side-dropdown-text">"Create client-winning proposals with open receipts, custom packages and electronic approvals"</div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a className="side-dropdown-link-block w-inline-block">
+                                            <div className="side-dropdown-inner-link">
+                                                <img />
+                                                <div className="side-dropdown-content">
+                                                    <div className="side-dropdown-heading">Contracts</div>
+                                                    <div className="side-dropdown-text">"Protect your business with fully-customizable contract templates. Edit and e-sign hassle free."</div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a className="side-dropdown-link-block w-inline-block">
+                                            <div className="side-dropdown-inner-link">
+                                                <img />
+                                                <div className="side-dropdown-content">
+                                                    <div className="side-dropdown-heading">Client CRM</div>
+                                                    <div className="side-dropdown-text">Add leads, manage existing clients and track projects easily fro one place</div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a className="side-dropdown-link-block w-inline-block">
+                                            <div className="side-dropdown-inner-link">
+                                                <img />
+                                                <div className="side-dropdown-content">
+                                                    <div className="side-dropdown-heading">Forms & Automations</div>
+                                                    <div className="side-dropdown-text">"Boost client intake and automate feedback with custom web forms and questionnaires"</div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a className="side-dropdown-link-block w-inline-block">
+                                            <div className="side-dropdown-inner-link">
+                                                <img />
+                                                <div className="side-dropdown-content">
+                                                    <div className="side-dropdown-heading">Time Tracking</div>
+                                                    <div className="side-dropdown-text">"Stay organized with Bonsai's all-in-one tracket, automated timesheets and billing system"</div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a className="side-dropdown-link-block w-inline-block">
+                                            <div className="side-dropdown-inner-link">
+                                                <img />
+                                                <div className="side-dropdown-content">
+                                                    <div className="side-dropdown-heading">Accounting</div>
+                                                    <div className="side-dropdown-text">"Manage your finances easily with income reports, expense tracking, tax reminders and more"</div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <a className="side-dropdown-link-block w-inline-block">
+                                            <div className="side-dropdown-inner-link">
+                                                <img />
+                                                <div className="side-dropdown-content">
+                                                    <div className="side-dropdown-heading">Task Tracking</div>
+                                                    <div className="side-dropdown-text">Stress-free task management with project templates and integrated timesheets</div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="main-nav-link relative">
+                            <div className="main-nav-dropdown-toggle">Templates</div>
+                            <div className="dropdown-arrow no-margin-right w-icon-dropdown-toggle"></div>
+                            <div className="main-nav-dropdown-wrapper-hidden">
+                                <div className="main-nav-left-content-templates">
+                                    <a className="main-nav-item---2 w-inline-block">
+                                        <div className="main-nav-item-wrapper">
+                                            <img />
+                                            <div className="template-main-nav-inside-dropdown-heading">Contract Templates</div>
+                                        </div>
+                                    </a>
+                                    <a className="main-nav-item---7 w-inline-block">
+                                        <div className="main-nav-item-wrapper">
+                                            <img />
+                                            <div className="template-main-nav-inside-dropdown-heading">Proposal Templates</div>
+                                        </div>
+                                    </a>
+                                    <a className="main-nav-item---1 w-inline-block">
+                                        <div className="main-nav-item-wrapper">
+                                            <img />
+                                            <div className="template-main-nav-inside-dropdown-heading">Invoice Templates</div>
+                                        </div>
+                                    </a>
+                                    <a className="main-nav-item---3 w-inline-block">
+                                        <div className="main-nav-item-wrapper">
+                                            <img />
+                                            <div className="template-main-nav-inside-dropdown-heading">Agreement Templates</div>
+                                        </div>
+                                    </a>
+                                    <a className="main-nav-item---4 w-inline-block">
+                                        <div className="main-nav-item-wrapper">
+                                            <img />
+                                            <div className="template-main-nav-inside-dropdown-heading">Quote Templates</div>
+                                        </div>
+                                    </a>
+                                    <a className="main-nav-item---8 w-inline-block">
+                                        <div className="main-nav-item-wrapper">
+                                            <img />
+                                            <div className="template-main-nav-inside-dropdown-heading">Scope of Work Templates</div>
+                                        </div>
+                                    </a>
+                                    <div className="main-nav-item---5">
+                                        <div className="main-nav-item-wrapper">
+                                            <img />
+                                            <div className="template-main-nav-inside-dropdown-heading">Brief Templates</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="main-nav-right-content-templates">
+                                    <div className="templates-right-content-item---1">
+                                        <div className="templates-right-wrapper">
+                                            <div className="main-nav-inside-dropdown-heading templates">Featured Invoice Templates</div>
+                                            <a className="templates-right-item bg1 w-inline-block">
+                                                <div className="template-main-nav-inside-dropdown-heading">Social Media Invoice Templates</div>
+                                            </a>
+                                            <a>
+                                                <div className="template-main-nav-inside-dropdown-heading">Contractor Invoice Template</div>
+                                            </a>
+                                            <a>
+                                                <div className="template-main-nav-inside-dropdown-heading">Web Design Invoice Template</div>
+                                            </a>
+                                            <a>
+                                                <div className="template-main-nav-inside-dropdown-heading">Influencer Invoice Template</div>
+                                            </a>
+                                        </div>
+                                        <a className="nav-green-text is-centered">See All Templates</a>
+                                    </div>
+                                    <div className="templates-right-content-item---2"></div>
+                                    <div className="templates-right-content-item---3"></div>
+                                    <div className="templates-right-content-item---4"></div>
+                                    <div className="templates-right-content-item---5"></div>
+                                    <div className="templates-right-content-item---6"></div>
+                                    <div className="templates-right-content-item---7"></div>
+                                    <div className="templates-right-content-item---8"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <a className="main-nav-link w--current">Pricing</a>
+                        <a className="main-nav-link hidden">Resource Hub</a>
+                        <a className="main-nav-link hidden">Reviews</a>
+                        <a className="main-nav-link is-last-item-hidden">Company</a>
+                        <img />
+                    </div>
+                    <div className="main-nav-right"></div>
+                    <div className="mobile-sub-menu-under-workflow"></div>
+                    <div className="mobile-sub-menu-workflow"></div>
+                    <div className="mobile-sub-menu-templates"></div>
+                </nav>
             </div>
         </div>
         <div></div>
