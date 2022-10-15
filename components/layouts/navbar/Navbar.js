1a6d8687b73a100d5635a354cba8513aa9b0939d
Sparkie95 <shind4r4_stalk@live.fr>
2022-10-15 22:26:13 +0200

    logo navbar

--- components/layouts/navbar/Navbar.js
+++ components/layouts/navbar/Navbar.js
@@ -1,3 +1,19 @@
 export default function Navbar() {
-    return <div>navbar</div>
+  return (
+    <div>
+      <div>
+        <div>
+            <div>
+                <a>
+                    <h1>FE2_BONSAI</h1>
+                </a>
+            </div>
+        </div>
+        <div></div>
+      </div>
+      <div>
+        <style></style>
+      </div>
+    </div>
+  );
 }
