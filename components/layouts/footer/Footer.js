361d5b158deed9f08d045a559765e6727d76ce33
Sparkie95 <shind4r4_stalk@live.fr>
2022-11-09 17:50:44 +0100

    footer 1st block

---
+++ components/layouts/footer/Footer.js
@@ -0,0 +1,48 @@
+export default function Footer() {
+    return (
+        <footer className="!bg-scroll maxmd:pb-[156px] maxmd:pt-[40px] maxlg:px-[5%] pt-[100px] px-[16%] pb-[96px]">
+            <div className="!bg-scroll maxmd:flex-wrap flex max-w-[1220px] mx-auto justify-between">
+                <div className="!bg-scroll maxmd:block maxmd:w-full maxmd:mt-[20px] maxmd:justify-between maxmd:flex-wrap maxmd:items-start w-[30%]">
+                    <div 
+                      className="!bg-scroll maxmd:w-full maxmd:mb-[25px] maxmd:text-[18px] mb-[40px] 
+                    text-[#4c4d5f] text-[17px] leading-[24px] font-semibold tracking-[0.12px] uppercase"
+                    >
+                        PRODUCT
+                    </div>
+                    <div className="!bg-scroll maxxs:w-full maxmd:w-[47%] flex mb-[25px] flex-col items-start">
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Proposals</a>
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Contracts</a>
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Invoicing</a>
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Client CRM</a>
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Time Tracking</a>
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Task Tracking</a>
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Forms</a>
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Accounting</a>
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Bonsai Tax</a>
+                        <a className="!bg-scroll maxmd:text-[16px] mb-[1px] text-[#4c4d5f] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">
+                            Bonsai Cash</a>
+                    </div>
+                    <div className="!bg-scroll maxxs:w-full maxmd:w-[47%] flex mb-[25px] flex-col items-start">
+                        <a className="text-[#00b289] !bg-scroll maxmd:text-[16px] mb-[1px] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">Pricing</a>
+                        <a className="text-[#4c4d5f] !bg-scroll maxmd:text-[16px] mb-[1px] leading-[30px] font-light tracking-[0.11px] no-underline cursor-pointer">Bonsai Reviews</a>
+                    </div>
+                </div>
+                <div className="!bg-scroll maxmd:block maxmd:w-full maxmd:mt-[20px] maxmd:justify-between maxmd:flex-wrap maxmd:items-start w-[30%]">
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </footer>
+    );
+}
