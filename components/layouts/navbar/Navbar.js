97b695ba46d0469b2938e161c108287d38c0b403
Nicolas Huynh <nisamzg@MacBook-Pro-de-Nicolas.local>
2022-11-12 23:54:57 +0100

    refacto

--- components/layouts/navbar/Navbar.js
+++ components/layouts/navbar/Navbar.js
@@ -1,6 +1,6 @@
-import React, { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import React from "react";
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import {
   NavbarProductNavigation,
   NavbarTemplatesNavigation,
